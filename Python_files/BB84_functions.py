7fbac29e112d110c28c111147cafc831170febdc
Lakshika Rathi <lakshika01022001@gmail.com>
2020-09-02 07:17:25 +0000

    Removed Extra functions

--- Python_files/BB84_functions.py
+++ Python_files/BB84_functions.py
@@ -4,34 +4,8 @@ from qiskit.visualization import plot_histogram, plot_bloch_multivector
 import numpy as np
 
 
-def bit_string1(n):
-    zeros = [0 for _ in range(n//2)]
-    ones = [1 for _ in range(n - n//2)]
-    bits = zeros + ones
-    np.random.shuffle(bits)
-    return bits
-
-
 def bit_string(n) :
     return randint(0, 2, n)
-
-
-def encode_bits1(bits, bases):
-    base_encoding = []
-    for bit, base in zip(bits, bases):
-        base_circuit = QuantumCircuit(1, 1)
-        if base == 0:
-            if bit == 1:
-                base_circuit.x(0)
-        if base == 1:
-            if bit == 0:
-                base_circuit.h(0)
-            if bit == 1:
-                base_circuit.x(0)
-                base_circuit.h(0)
-        base_circuit.barrier()
-        base_encoding.append(base_circuit)
-    return base_encoding
 
 
 def encode_bits(bits, bases):
@@ -49,21 +23,6 @@ def encode_bits(bits, bases):
                 base_circuit.h(i)
     base_circuit.barrier()
     return base_circuit
-
-
-def measure_bits1(circuit, bases):
-    backend = Aer.get_backend('qasm_simulator')
-    measured_bits = []
-    for j in range(len(bits)):
-        if bases[j] == 0:
-            circuit[j].measure(0,0)
-        if bases[j] == 1:
-            circuit[j].h(0)
-            circuit[j].measure(0,0)
-        result = execute(circuit[j], backend, shots=1, memory = True).result()
-        measured_bit = int(result.get_memory()[0])
-        measured_bits.append(measured_bit)
-    return measured_bits
 
 
 def measure_bits(circuit, bases):
